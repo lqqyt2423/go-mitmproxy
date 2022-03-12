6d5ce792ce8e76db92f915eb6969696be1f8224e
ZhouYixun <291028775@qq.com>
2022-03-12 22:53:49 +0800

    更改表宽度自适应

--- addon/web/client/src/App.tsx
+++ addon/web/client/src/App.tsx
@@ -185,7 +185,7 @@ class App extends React.Component<IProps, IState> {
               <th style={{ width: '50px' }}>No</th>
               <th style={{ width: '80px' }}>Method</th>
               <th style={{ width: '200px' }}>Host</th>
-              <th style={{ width: '600px' }}>Path</th>
+              <th style={{ width: 'auto' }}>Path</th>
               <th style={{ width: '150px' }}>Type</th>
               <th style={{ width: '80px' }}>Status</th>
               <th style={{ width: '90px' }}>Size</th>
