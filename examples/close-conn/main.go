486fdc7b45c37a09b0b620ed38cb287f6d19a614
lqqyt2423 <974923609@qq.com>
2024-02-23 17:10:51 +0800

    not connect or delay connect target server example

--- examples/close-conn/main.go
+++ examples/close-conn/main.go
@@ -5,6 +5,24 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+//
+// When the proxy forwards HTTPS requests, go-mitmproxy by default initiates an SSL connection with the target server first.
+// 1. If you do not want to establish a connection with the target server, such as:
+//    Generating a response directly in the RequestHeaders or Request Hook and returning it to the client.
+// 2. Or if you want the proxy to establish a connect connection with the client first, delaying the connection with the target service until a real HTTPS request is made.
+//
+// => Then you can refer to the following code
+//    set client.UpstreamCert = false in the ClientConnected Hook.
+
+//
+// 当代理 https 请求时，go-mitmproxy 默认会先和目标服务器建立 SSL 连接
+// 1. 如果你并不和目标服务器建立连接，如：
+//    在 Requestheaders 或 Request Hook 中直接生成 Response，返回给客户端
+// 2. 或者想让中间人先和客户端成功建立 connect 连接，推迟至真正发起 https 请求时中间人和目标服务再建立连接
+//
+// => 那么你可以参考下面代码
+//    在 ClientConnected Hook 中设置 client.UpstreamCert = false
+
 type CloseConn struct {
 	proxy.BaseAddon
 }
