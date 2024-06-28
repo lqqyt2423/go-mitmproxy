298cd93a1d978ebf25361a05a85a8e98536a905e
lqqyt2423 <974923609@qq.com>
2024-06-28 13:33:20 +0800

    web remove web-vitals

--- web/client/src/index.tsx
+++ web/client/src/index.tsx
@@ -2,12 +2,6 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 root.render(<App />)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
