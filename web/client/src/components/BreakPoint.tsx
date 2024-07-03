89fa6a700a7f79c08627026f39d804e490dd4ecc
lqqyt2423 <974923609@qq.com>
2024-07-03 15:41:32 +0800

    web refactor BreakPoint

--- web/client/src/components/BreakPoint.tsx
+++ web/client/src/components/BreakPoint.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
 import Form from 'react-bootstrap/Form'
@@ -13,47 +13,20 @@ interface IRule {
   action: Action
 }
 
-interface IState {
-  show: boolean
-  rule: IRule
-  haveRules: boolean
-}
-
 interface IProps {
   onSave: (rules: IRule[]) => void
 }
 
-class BreakPoint extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props)
+function BreakPoint({ onSave }: IProps) {
+  const [show, setShow] = useState(false)
+  const [rule, setRule] = useState<IRule>({ method: 'ALL', url: '', action: 1 })
+  const [haveRules, setHaveRules] = useState(false)
 
-    this.state = {
-      show: false,
+  const variant = haveRules ? 'success' : 'primary'
 
-      rule: {
-        method: 'ALL',
-        url: '',
-        action: 1,
-      },
-
-      haveRules: false,
-    }
-
-    this.handleClose = this.handleClose.bind(this)
-    this.handleShow = this.handleShow.bind(this)
-    this.handleSave = this.handleSave.bind(this)
-  }
-
-  handleClose() {
-    this.setState({ show: false })
-  }
-
-  handleShow() {
-    this.setState({ show: true })
-  }
-
-  handleSave() {
-    const { rule } = this.state
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+  const handleSave = () => {
     const rules: IRule[] = []
     if (rule.url) {
       rules.push({
@@ -62,69 +35,62 @@ class BreakPoint extends React.Component<IProps, IState> {
         action: rule.action,
       })
     }
-
-    this.props.onSave(rules)
-    this.handleClose()
-
-    this.setState({ haveRules: rules.length ? true : false })
+    onSave(rules)
+    handleClose()
+    setHaveRules(rules.length ? true : false)
   }
 
-  render() {
-    const { rule, haveRules } = this.state
-    const variant = haveRules ? 'success' : 'primary'
+  return (
+    <div>
+      <Button variant={variant} size="sm" onClick={handleShow}>BreakPoint</Button>
 
-    return (
-      <div>
-        <Button variant={variant} size="sm" onClick={this.handleShow}>BreakPoint</Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Set BreakPoint</Modal.Title>
+        </Modal.Header>
 
-        <Modal show={this.state.show} onHide={this.handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Set BreakPoint</Modal.Title>
-          </Modal.Header>
+        <Modal.Body>
+          <Form.Group as={Row}>
+            <Form.Label column sm={2}>Method</Form.Label>
+            <Col sm={10}>
+              <Form.Control as="select" value={rule.method} onChange={e => { setRule({ ...rule, method: e.target.value as Method }) }}>
+                <option>ALL</option>
+                <option>GET</option>
+                <option>POST</option>
+                <option>PUT</option>
+                <option>DELETE</option>
+              </Form.Control>
+            </Col>
+          </Form.Group>
 
-          <Modal.Body>
-            <Form.Group as={Row}>
-              <Form.Label column sm={2}>Method</Form.Label>
-              <Col sm={10}>
-                <Form.Control as="select" value={rule.method} onChange={e => { this.setState({ rule: { ...rule, method: e.target.value as Method } }) }}>
-                  <option>ALL</option>
-                  <option>GET</option>
-                  <option>POST</option>
-                  <option>PUT</option>
-                  <option>DELETE</option>
-                </Form.Control>
-              </Col>
-            </Form.Group>
+          <Form.Group as={Row}>
+            <Form.Label column sm={2}>URL</Form.Label>
+            <Col sm={10}><Form.Control value={rule.url} onChange={e => { setRule({ ...rule, url: e.target.value }) }} /></Col>
+          </Form.Group>
 
-            <Form.Group as={Row}>
-              <Form.Label column sm={2}>URL</Form.Label>
-              <Col sm={10}><Form.Control value={rule.url} onChange={e => { this.setState({ rule: { ...rule, url: e.target.value } }) }} /></Col>
-            </Form.Group>
+          <Form.Group as={Row}>
+            <Form.Label column sm={2}>Action</Form.Label>
+            <Col sm={10}>
+              <Form.Control as="select" value={rule.action} onChange={e => { setRule({ ...rule, action: parseInt(e.target.value) as Action }) }}>
+                <option value="1">Request</option>
+                <option value="2">Response</option>
+                <option value="3">Both</option>
+              </Form.Control>
+            </Col>
+          </Form.Group>
+        </Modal.Body>
 
-            <Form.Group as={Row}>
-              <Form.Label column sm={2}>Action</Form.Label>
-              <Col sm={10}>
-                <Form.Control as="select" value={rule.action} onChange={e => { this.setState({ rule: { ...rule, action: parseInt(e.target.value) as Action } }) }}>
-                  <option value="1">Request</option>
-                  <option value="2">Response</option>
-                  <option value="3">Both</option>
-                </Form.Control>
-              </Col>
-            </Form.Group>
-          </Modal.Body>
-
-          <Modal.Footer>
-            <Button variant="secondary" onClick={this.handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={this.handleSave}>
-              Save
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
-    )
-  }
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleSave}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  )
 }
 
 export default BreakPoint
